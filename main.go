fb654762c92ec0b63981123284bef18764ae4b78
i-DarkLight <yusefyp30@gmail.com>
2023-12-18 10:16:16 +0330

    added function to prevent f/f error

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"strings"
 	"github.com/i-DarkLight/NRParser/first"
 	"github.com/i-DarkLight/NRParser/follow"
 )
 
+func catchError() bool {
+	for key:=range first.MapTerminal{
+		first.FindErr(key)
+	}
+	for key, value := range first.FirstErr {
+		if strings.Contains(value, key) {
+			return true
+		}
+	}
+	return false
+}
 func main() {
-	first.FindAllTogether("A -> aAs | nCb | BcD | BDg\nB -> ko | pAo | ~\nC -> qr | jAu\nD -> l | ~")
+	str := "A -> aAs | nCb | BcD\nB -> xo | yo | ~\nC -> Br | jAu\nD -> l | ~"
+	lines := strings.Split(str, "\n")
+	//save non terminals as key and their respective options as value in a map
+	for _, elem := range lines {
+		pos := strings.Index(elem, "->")
+		first.MapTerminal[elem[:pos-1]] = elem[pos+3:]
+	}
+	catchError()
+	flag := false
+	for range lines {
+		if catchError() {
+			flag = true
+		}
+	}
+	if !flag {
+		first.FindAllTogether()
+	} else {
+		println("there is a first/first problem in your grammar")
+	}
 	follow.FindFollow()
 	println("================================")
 	for key, value := range follow.FollowMap {
